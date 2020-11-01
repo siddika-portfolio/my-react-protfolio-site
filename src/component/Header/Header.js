40287f654dc969ea57ae9cf71ab6a998bf9ef14a
Siddika-akhter <siddika.tas28@gmail.com>
2020-11-01 08:50:13 +0600

    description change

--- src/component/Header/Header.js
+++ src/component/Header/Header.js
@@ -15,7 +15,7 @@ const Header = () => {
                             <img className="img-fluid" src={protfolioImg} alt="" />
                         </div>
                     </div>
-                    <div className="col-md-6 title-des">
+                    <div className="col-md-6 title-des d-flex align-items-center">
                         <div className="header_content mt-3 align-items-center">
                             <h2>Hi, This is </h2>
                             <h1> Siddika Akhter</h1>
@@ -23,17 +23,6 @@ const Header = () => {
                             <a className="nav-link mt-4" href="https://drive.google.com/file/d/1s7AGavyifV01JTImVaHGZQQlMh86rl6Y/view?usp=sharing">
                                 <div className="btn btn mb-3" id="navButton">Download CV</div>
                             </a>
-                            {/* <div className="banner-des mt-3">
-                                <p>I count myself as a hardworking person. To pursue a challenging career in a</p>
-
-                                <p>competitive world where enthusiasm, honesty, hard work and passion will support me to</p>
-
-                                <p>grow myself as well as utilize my knowledge and expertise for</p>
-
-                                <p>professional development in the relevant field.</p>
-
-                                <p>Email: siddika.tas28@gmail.com</p>
-                            </div> */}
                             <div className="header_icons mt-3">
                                 <a href=""><FaFacebookF /></a>
                                 <a href=""><FaLinkedinIn /></a>
